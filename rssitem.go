94bc8afa1ec4c75ddaf39a39005fb341357cce3b
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2017-01-02 12:18:20 +0100

    id, uud and dates added /BB

--- rssitem.go
+++ rssitem.go
@@ -3,12 +3,15 @@ package rsslib
 import ()
 
 type RssItem struct {
-	Id          int64  `json:"id"`
+	Id          int64  `json:"Id"`
+	Uuid        string `json:"Uuid"`
 	Channel     string `json:"Channel"`
 	Title       string `json:"Title"`
 	Link        string `json:"Link"`
 	Description string `json:"Description"`
 	Thumbnail   string `json:"Thumbnail"`
+	PublishDate string `json:"PublishDate"`
+	UpdateDate  string `json:"UpdateDate"`
 }
 
 type RssItems []RssItem
